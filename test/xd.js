0e8335b0497d7fb73f79a2ae4149fb0ce664f8c0
JOSUERV99 <josuerojasvega@gmail.com>
2021-11-15 22:52:48 -0600

    .

---
+++ test/xd.js
@@ -0,0 +1,8 @@
+
+var assert = require('assert');
+
+describe('Simple test', function () {
+ it('should return number of charachters in a string', function () {
+        assert.equal("Hello".length, 5);
+    });
+});
