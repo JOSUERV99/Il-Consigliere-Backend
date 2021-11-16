6752425c8dc592f21025e2c9279713967dd0796c
JOSUERV99 <josuerojasvega@gmail.com>
2021-11-15 23:10:58 -0600

    .

--- frontend/src/components/Consejos/Consejos.test.js
+++ frontend/src/components/Consejos/Consejos.test.js
@@ -2,6 +2,12 @@ import assert from 'assert';
 import React from 'react';
 import renderer from 'react-test-renderer';
 
-test('Simple test', () => {
-  assert(10 < 11);
-});
+describe('Basic Mocha String Test', () => {
+  it('should return number of charachters in a string', () => {
+         assert.equal("Hello".length, 5);
+     });
+  it('should return first charachter of the string', () =>  {
+         assert.equal("Hello".charAt(0), 'H');
+         //throw {myError:'throwing error to fail test'}
+     });
+ });
