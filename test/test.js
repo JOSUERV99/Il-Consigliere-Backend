eeb0e78713555d6e393039b8e5e4087833e278ee
Kevin Fallas <kf.ma@estudiantec.cr>
2021-11-17 19:06:43 -0600

    Test 1

--- test/test.js
+++ test/test.js
@@ -2,18 +2,66 @@ let chai = require('chai');
 let chaiHttp = require('chai-http');
 var assert = require('assert');
 
-const { expect } = require('chai');
+const expect = require('chai').expect;
+const nock = require('nock');
 let should = chai.should();
 require('dotenv').config();
 
 chai.use(chaiHttp);
 
-describe('Basic Mocha String Test', function () {
- it('should return number of charachters in a string', function () {
-        assert.equal("Hello".length, 5);
+const db = require('../database/models');
+
+const body_1 = {
+    "cedula": "44444",
+    "nombre": "Luis", 
+    "apellido": "Fuentes", 
+    "segundo_apellido": "Gonzalez", 
+    "clave": "1234", 
+    "id_tipo_convocado": 1
+}
+
+const body_2 = {
+    "cedula": "44444",
+    "nombre": "Luis Mariano", 
+    "apellido": "Fuentes", 
+    "segundo_apellido": "Gonzalez", 
+    "clave": "1234", 
+    "id_tipo_convocado": 1
+}
+
+const body_3 = {
+    "cedula": "144444",
+    "nombre": "Luis Mariano", 
+    "apellido": "Fontanera", 
+    "segundo_apellido": "Gonzalez", 
+    "clave": "1234", 
+    "id_tipo_convocado": 1
+}
+
+describe('Modificar usuario',  () => {
+    const ced = 44444;
+
+    it('Escenario 1: Cambio de nombre', async () => {
+        request
+        .put(`/usuario/${ced}`)
+        .send(body_1)
+        .expect(200)
+        .end(done);
     });
- it('should return first charachter of the string', function () {
-        assert.equal("Hello".charAt(0), 'H');
-        //throw {myError:'throwing error to fail test'}
+    it('Escenario 2: Cambio de apellido', async () => {
+        request
+        .put(`/usuario/${ced}`)
+        .send(body_2)
+        .expect(200)
+        .end(done);
     });
+
+    it('Escenario 3: Cambio de cedula', async () => {
+        request
+        .put(`/usuario/${ced}`)
+        .send(body_3)
+        .expect(200)
+        .end(done);
+    });
+    
 });
--- test/xd.js
+++
@@ -1,8 +0,0 @@
-
-var assert = require('assert');
-
-describe('Simple test', function () {
- it('should return number of charachters in a string', function () {
-        assert.equal("Hello".length, 5);
-    });
-});
